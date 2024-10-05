a22209e342a6de719339cd049625ff423be299be
Ayoub hj <medayoubhajamor197@gmail.com>
2024-10-05 12:57:13 +0100

    Add a function that generates food containers

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,26 @@
+function foodGenerateDev(food) {
+    let container = document.createElement("div");
+    container.setAttribute("class", "food");
+    
+    
+    let foodImageContainer = document.createElement("div");
+    let foodImage = document.createElement("img");
+    foodImage.setAttribute("src", food.img);
+    foodImageContainer.appendChild(foodImage);
+
+    let title = document.createElement("div");
+    title.setAttribute("class", "food-title");
+    title.innerHTML = food.text;
+    
+    
+    let price = document.createElement("div");
+    price.innerHTML = food.price;
+
+    container.appendChild(foodImageContainer);
+    container.appendChild(title);
+    container.appendChild(price);
+}
+
 function LoadMenuPage() {
     let title = document.createElement("div");
     title.innerHTML = "Menu";
