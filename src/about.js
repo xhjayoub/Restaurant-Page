b17de35989249f4e0de735f549b6c62bf75b778b
Ayoub hj <medayoubhajamor197@gmail.com>
2024-10-05 15:31:22 +0100

    Add contact to about page

--- src/about.js
+++ src/about.js
@@ -3,10 +3,23 @@ function LoadAboutPage() {
     title.innerHTML = "About";
     title.setAttribute("class", "menuTitle");
 
+    let contact = document.createElement("div");
+    contact.setAttribute("class", "contact");
+    let name = document.createElement("div");
+    name.innerHTML = "Boss";
+    let num = document.createElement("div");
+    num.innerHTML = "555-555-5554";
+    let email = document.createElement("div");
+    email.innerHTML = "totallyRealEmail@notFake.com";
+
+    contact.appendChild(name);
+    contact.appendChild(num);
+    contact.appendChild(email);
 
     content.innerHTML = "";
 
     content.appendChild(title);
+    content.appendChild(contact);
 }
 
 export { LoadAboutPage }; 
