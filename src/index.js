41b6149cc27e724aa40683bec3e6f9c205839521
Ayoub hj <medayoubhajamor197@gmail.com>
2024-10-05 11:37:11 +0100

    Add event listener to home button to load homepage

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import img from "./images/Flag_of_Tunisia.png";
 import { LoadHomePage } from "./home.js";
 
 LoadHomePage();
+document.querySelector("#home").addEventListener("click", LoadHomePage);
